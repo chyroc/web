41aa9ab53b064211564be3eeabda6688c4b367de
Chyroc <chen_yunpeng@foxmail.com>
2018-06-21 20:30:58 +0800

    remove examples to github/webapi-examples

--- examples/pure-wasm/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd examples/pure-wasm
-
-GOARCH=wasm GOOS=js /usr/local/Cellar/go/dev/bin/go build -o example.wasm github.com/Chyroc/webapi/examples/pure-wasm && serve
--- examples/webapi/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd examples/webapi
-
-GOARCH=wasm GOOS=js /usr/local/Cellar/go/dev/bin/go build -o example.wasm github.com/Chyroc/webapi/examples/webapi && serve
