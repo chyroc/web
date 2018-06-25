98e4d4ac66543a9f618f68908da440742d4c09bd
Chyroc <chen_yunpeng@foxmail.com>
2018-06-25 21:04:06 +0800

    update

--- html_select_element.go
+++ html_select_element.go
@@ -7,7 +7,15 @@ import "syscall/js"
 type HTMLSelectElement interface {
 	HTMLElement
 
+	// value
 	Value() string
+	SetValue(s string)
+	// multiple
+	Multiple() bool
+	SetMultiple(s bool)
+	// required
+	Required() bool
+	SetRequired(s bool)
 }
 
 func newImplHTMLSelectElement(v js.Value) implHTMLSelectElement {
@@ -27,4 +35,26 @@ type implHTMLSelectElement struct {
 
 func (r *implHTMLSelectElement) Value() string {
 	return r.v.Get("value").String()
+}
+
+func (r *implHTMLSelectElement) SetValue(s string) {
+	r.v.Set("value", s)
+}
+
+// Sets or retrieves the Boolean value indicating whether multiple items can be selected from a list.
+func (r *implHTMLSelectElement) Multiple() bool {
+	return r.v.Get("multiple").Bool()
+}
+
+func (r *implHTMLSelectElement) SetMultiple(s bool) {
+	r.v.Set("multiple", s)
+}
+
+// When present, marks an element that can't be submitted without a value.
+func (r *implHTMLSelectElement) Required() bool {
+	return r.v.Get("required").Bool()
+}
+
+func (r *implHTMLSelectElement) SetRequired(s bool) {
+	r.v.Set("required", s)
 }
